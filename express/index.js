24ab88d6b4d879dce6a89ba7067ddff8686ba89a
Paalan <42642576+paalamugan@users.noreply.github.com>
2021-09-20 12:44:45 +0530

    Modified redirect api with regrex expression

--- express/index.js
+++ express/index.js
@@ -8,12 +8,14 @@ const { setSecurityHeaders, loggerApi } = require('../middleware');
 const { apiErrorHandler } = require('../error-handler/api-error-handler');
 const config = require('../config');
 
+const clientPath = path.join(__dirname, '..', 'public');
+
 module.exports = (app) => {
 
     app.set('env', config.env);
     app.set('port', config.port);
 
-    app.enable('trust proxy', 1);
+    app.enable('trust proxy');
     app.disable('x-powered-by');
 
     // middleware
@@ -23,15 +25,19 @@ module.exports = (app) => {
     app.use(loggerApi);
     app.use(setSecurityHeaders);
 
-    app.use((req, res, next) => {
-        if (!path.extname(req.path) && !req.path.includes('/api/')) {
-            req.url = '/index.html';
-        }
-        return next();
+//     app.use((req, res, next) => {
+//         if (!path.extname(req.path) && !req.path.includes('/api/')) {
+//             req.url = '/index.html';
+//         }
+//         return next();
+//     });
+    
+    app.use(express.static(clientPath));
+
+    app.use(/^(?!\/api).+/, (req, res, next) => {
+        res.sendFile(`${clientPath}/index.html`);
     });
     
-    app.use(express.static(path.join(__dirname, '..', 'public')));
-
     // Initialize routes
     require('../routes')(app);
 
